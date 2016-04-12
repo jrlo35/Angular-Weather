640c75990424878841f64dbb2f8507978851866c
Jonathon Lo <jonathon.lo35@gmail.com>
2016-04-12 16:05:58 -0700

    Initialize app with relevant files and folders

---
+++ app.js
@@ -0,0 +1,30 @@
+var weatherapp = angular.module('weatherApp',['ngRoute', 'ngResource']);
+
+
+
+weatherApp.config(function($routeProvider){
+
+	$routeProvider
+	.when('/', {
+		templateUrl: 'pages/home.html',
+		controller: 'homeController'
+	})
+	.when('/forecast', {
+		templateUrl: 'pages/forecast.html',
+		controller: 'forecastController'
+	})
+})
+
+
+
+weatherApp.controller('homeController',['$scope', function($scope) {
+
+
+
+}])
+
+weatherApp.controller('forecastController',['$scope', function($scope) {
+
+
+	
+}])
