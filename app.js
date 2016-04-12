3b5044356a3b1933793ef14974cad39e785a09da
Jonathon Lo <jonathon.lo35@gmail.com>
2016-04-12 16:53:56 -0700

    Add forecast routes

--- app.js
+++ app.js
@@ -16,15 +16,23 @@ weatherApp.config(function($routeProvider){
 })
 
 
+weatherApp.service('cityService', function() {
+	this.city = 'New York, NY';
+})
 
-weatherApp.controller('homeController',['$scope', function($scope) {
 
+weatherApp.controller('homeController',['$scope','cityService', function($scope, cityService) {
+  
+  $scope.city = cityService.city;
+  $scope.$watch('city', function (){
+  	cityService.city = $scope.city
+  })
 
 
 }])
 
-weatherApp.controller('forecastController',['$scope', function($scope) {
+weatherApp.controller('forecastController',['$scope', 'cityService', function($scope ,cityService) {
 
-
+  $scope.city = cityService.city;
 	
 }])
